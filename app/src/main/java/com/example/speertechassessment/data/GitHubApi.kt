44c98961f51810ddfb623c1ab7cfdc0fd224f443
MrBE <essienedim@gmail.com>
2025-04-05 00:14:44 -0400

    chore: Initialize Retrofit resources

---
+++ app/src/main/java/com/example/speertechassessment/data/GitHubApi.kt
@@ -0,0 +1,45 @@
+package com.example.speertechassessment.data
+
+import retrofit2.Retrofit
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+//Holds user data
+data class GitHubUser(
+    val login: String,
+    val avatar: String,
+    val name: String?,
+    val bio: String?,
+    val followers: Int,
+    val following: Int
+
+)
+
+
+//API interface with retrofit
+interface GitHubApi{
+
+    @GET("users/{username}")    //return Github user object from query
+    suspend fun getUser(@Path("username") username:String) : GitHubUser
+
+    @GET("users/{username}/followers")    //return list of Github followers users object from query
+    suspend fun getFollowers(@Path("username") username:String) : List<GitHubUser>
+
+    @GET("users/{username}/following")    //return list of Github followers users object from query
+    suspend fun getFollowing(@Path("username") username:String) : List<GitHubUser>
+
+}
+
+
+/* Retrofit Setup */
+
+object RetrofitInstance {
+    val api: GitHubApi by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .build()
+            .create(GitHubApi::class.java)
+    }
+}
