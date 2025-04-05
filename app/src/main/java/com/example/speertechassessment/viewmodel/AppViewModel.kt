7799b24a5f1bb83db401846630b56c07a7382759
MrBE <essienedim@gmail.com>
2025-04-05 01:23:16 -0400

    test: Set up MainViewModel setup with test

--- app/src/main/java/com/example/speertechassessment/viewmodel/AppViewModel.kt
+++ app/src/main/java/com/example/speertechassessment/viewmodel/AppViewModel.kt
@@ -1,4 +1,35 @@
 package com.example.speertechassessment.viewmodel
 
-class AppViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.speertechassessment.data.GitHubUser
+import com.example.speertechassessment.data.RetrofitInstance
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+sealed class UiState { //cover all possibilities
+    object Idle: UiState()
+    object Loading: UiState()
+    data class Success (val user: GitHubUser): UiState()
+    object  NotFound:UiState()
+}
+
+class AppViewModel: ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState>(UiState.Idle) //innit (internal)
+    val uiState:StateFlow<UiState> = _uiState
+
+    fun getGitHubUser(username: String) {
+        viewModelScope.launch {
+            _uiState.value  = UiState.Loading
+
+            try {
+                val user = RetrofitInstance.api.getUser(username)
+                _uiState.value = UiState.Success(user)
+            } catch (e:Exception){
+                _uiState.value = UiState.NotFound
+            }
+        }
+    }
 }
---
+++ app/src/test/java/com/example/speertechassessment/viewmodel/AppViewModelTest.kt
@@ -0,0 +1,54 @@
+package com.example.speertechassessment.viewmodel
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@get:Rule
+
+val dispatcher = UnconfinedTestDispatcher()
+class AppViewModelTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setup() {
+        Dispatchers.setMain(dispatcher)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+
+    @Test
+    fun testInitialAppStateIsIdle() {
+        val viewModel = AppViewModel()
+        kotlin.run {
+            assertEquals(UiState.Idle, viewModel.uiState.value)
+        }
+    }
+
+
+    @Test
+    fun testGetGitHubUserState() = runTest{
+        val viewModel = AppViewModel()
+
+        viewModel.getGitHubUser("Mr-BE")
+
+            assert(viewModel.uiState.value !is UiState.Idle)
+
+
+    }
+}
